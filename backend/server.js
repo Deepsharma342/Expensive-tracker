4bc9579ed525736698ece554622177885dbe1691
full-stack <deepanshusharma66086@gmail.com>
2026-04-05 10:56:01 +0530

    final res

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://glittering-capybara-93ed98.netlify.app/api",
+  origin: "https://fascinating-vacherin-dbed93.netlify.app/api",
   credentials: true
 }));
 
